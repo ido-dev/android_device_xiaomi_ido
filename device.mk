e75d2238bdd1bde1bf7d035d9801f20b5f58390c
Anik khan <anik9280@gmail.com>
2016-02-11 13:49:05 +0600

    ido: Add Display configuration

--- device.mk
+++ device.mk
@@ -59,3 +59,11 @@ PRODUCT_COPY_FILES += \
 # Power HAL
 PRODUCT_PACKAGES += \
     power.msm8916
+
+# Display
+PRODUCT_PACKAGES += \
+    copybit.msm8916 \
+    gralloc.msm8916 \
+    hwcomposer.msm8916 \
+    libtinyxml \
+    memtrack.msm8916
