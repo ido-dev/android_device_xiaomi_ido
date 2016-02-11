3364b2a389da83c5683e6ea9d1fc784fa00bff01
Anik khan <anik9280@gmail.com>
2016-02-11 13:42:13 +0600

    ido: Use Qcom PowerHAL

--- device.mk
+++ device.mk
@@ -55,3 +55,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    power.msm8916
