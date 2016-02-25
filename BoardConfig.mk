6ff943275d28882b672984267b7637d0b3a1e5ee
Anik khan <anik9280@gmail.com>
2016-02-25 10:45:11 +0600

    ido: Set proper kernel information

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,12 +43,13 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
-BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_CMDLINE := console=115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevic
+e=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET     := 0x01000000
+BOARD_RAMDISK_OFFSET     := 0x02000000
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 
 # ANT+
