16af3bf30f31ab21a18d0ca4ad760c9595972d89
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-04-24 16:30:43 +0800

    ido: Add keylayouts
    Change-Id: Iedde7d345d444dfa3d26c786c59fa5e3ba86d9ac

--- device.mk
+++ device.mk
@@ -111,6 +111,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
+
 # Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8916
