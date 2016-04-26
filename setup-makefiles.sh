6c711281b6c58deebe76568392a010e364e280a4
Anik1199 <anik9280@gmail.com>
2016-04-26 15:42:05 +0600

    ido: Use prebuilt modules

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -230,9 +230,6 @@ LOCAL_MULTILIB := 64
 LOCAL_PROPRIETARY_MODULE := true
 include \$(BUILD_PREBUILT)
 
-\$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-\$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib64/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-
 endif
 endif
 
