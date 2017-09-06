25cc2774c5d24b8ac2b8e9a8cd4148d194e7b7d1
Anik1199 <anik@lineageos.org>
2017-09-06 23:08:42 +0600

    ido: Add libbase dependency for init

--- init/Android.mk
+++ init/Android.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,5 +22,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_SRC_FILES := init_ido.cpp
 LOCAL_MODULE := libinit_ido
+LOCAL_STATIC_LIBRARIES := \
+    libbase
 
 include $(BUILD_STATIC_LIBRARY)
